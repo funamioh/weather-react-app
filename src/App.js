02bc6966f5cd06685931f4b15736b9fcbe5f524f
Miho F <85437491+funamioh@users.noreply.github.com>
2021-10-17 23:18:17 +0900

    Added axios integration and function so it shows weather information

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
+      <footer>
+        <p>This project is coded by Miho Funayama and <a>open-sourced on GitHub</a>. Also hosted on Netlify</p>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import './App.css';
+import axios from "axios";
+
+export default function Weather(props) {
+    const [city, setCity] = useState("");
+    //whether or not there is a result
+    const [loaded, setLoaded] = useState(false);
+    const [weather, setWeather] = useState({});
+    
+    function showWeather(response) {
+        setLoaded(true);
+        setWeather({
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            wind:response.data.wind.speed,
+            humidity: response.data.main.humidity,
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+        })
+
+    }
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+        let apiKey = `730afeb398d3874cb3c0cb8d98df8b85`;
+        let units = "metric"
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+        axios.get(apiUrl).then(showWeather);
+    }
+    
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
+    let form = <form onSubmit={handleSubmit}><input type="search" placeholder="Enter a cifty" onChange={updateCity}/>
+    <input type="submit" value="Search"/>
+    <button>Current</button>
+    </form>;
+
+    if (loaded) {
+        return (
+            <div>
+            {form}
+            <ul>
+            <li>Temperature: {Math.round(weather.temperature)}℃</li>
+            <li>Description: {weather.description}</li>
+            <li>Humidity: {weather.humidity}%</li>
+            <li>Wind speed: {weather.wind}km/h</li>
+            <li><img src={weather.icon} alt="weather icon" />
+            </li>
+            </ul>
+            </div>
+        );
+    } else {
+    return form;
+}
+}

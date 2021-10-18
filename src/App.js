61cc31990a88fdb58140e9ba2020d15cb4639b71
Miho F <85437491+funamioh@users.noreply.github.com>
2021-10-18 22:23:15 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <Weather />
       <footer>
         This project is coded by Miho Funayama and <a href="https://github.com/funamioh/weather-react-app" target="_blank">open-sourced on GitHub</a>. Also hosted on <a href="https://trusting-franklin-037844.netlify.app" target="_blank">Netlify</a>
       </footer>
+      </div>
     </div>
   );
 }

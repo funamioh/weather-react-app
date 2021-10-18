75d06eef55bb841c3635d6895ad4b53a6ddd54fb
Miho F <85437491+funamioh@users.noreply.github.com>
2021-10-18 23:35:22 +0900

    Addressed what are mentioned on warnings

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="container">
       <Weather />
       <footer>
-        This project is coded by Miho Funayama and <a href="https://github.com/funamioh/weather-react-app" target="_blank">open-sourced on GitHub</a>. Also hosted on <a href="https://trusting-franklin-037844.netlify.app" target="_blank">Netlify</a>
+        This project is coded by Miho Funayama and <a href="https://github.com/funamioh/weather-react-app" target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a>. Also hosted on <a href="https://trusting-franklin-037844.netlify.app" target="_blank" rel="noopener noreferrer">Netlify</a>
       </footer>
       </div>
     </div>

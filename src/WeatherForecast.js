e5c62430273adb996bfc2265712d85a3e2b99aa9
Miho F <85437491+funamioh@users.noreply.github.com>
2021-10-31 17:59:24 +0900

    created load() function to make the code more readable

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -11,14 +11,21 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
     }
-
+    // if the coordinates change
+    //set loaded false
     useEffect(() => {
       setLoaded(false);
     }, [props.coordinates]);
     
-    // if the coordinates change
-    //set loaded false
+function load() {
+    let apiKey = `b16e4cc8e76040cdfe4ae29b0af21854`
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   
+  axios.get(apiUrl).then(handleResponse);
+}
+
   if (loaded) {
   return <div className="WeatherForecast"> 
     <div className="row">
@@ -29,18 +36,13 @@ export default function WeatherForecast(props) {
         <WeatherForecastDay data={dailyForecast} />
       </div>
         );
+        } else {
+          return null;
         }
       })}
       </div>
       </div>
   } else {
-    let apiKey = `b16e4cc8e76040cdfe4ae29b0af21854`
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  
-  axios.get(apiUrl).then(handleResponse);
-
-  return null;
-  }
+    load();
+}
 }

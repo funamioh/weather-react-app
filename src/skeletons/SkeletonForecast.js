d0f8cc76d197cf14a8d003e12e12cf1c3e456ba8
Miho F <85437491+funamioh@users.noreply.github.com>
2021-12-27 21:59:02 +0900

    Add Skeleton component

--- src/skeletons/SkeletonForecast.js
+++ src/skeletons/SkeletonForecast.js
@@ -3,15 +3,74 @@ import "./SkeletonLoading.css";
 
 const SkeletonForecast = () => {
   return (
-    <div>
-      <div className="skeleton-day">
-        <Skeleton count={1} height={20} width={50} />
-      </div>
-      <div className="skeleton-forecast-icon">
-        <Skeleton count={1} height={50} width={50} />
-      </div>
-      <div className="skeleton-temp">
-        <Skeleton count={1} height={20} width={50} />
+    <div className="SkeletonForecast">
+      <div className="row">
+        <div className="col-2">
+          <div className="skeleton-day">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+          <div className="skeleton-forecast-icon">
+            <Skeleton count={1} height={50} width={50} />
+          </div>
+          <div className="skeleton-temp">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="skeleton-day">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+          <div className="skeleton-forecast-icon">
+            <Skeleton count={1} height={50} width={50} />
+          </div>
+          <div className="skeleton-temp">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="skeleton-day">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+          <div className="skeleton-forecast-icon">
+            <Skeleton count={1} height={50} width={50} />
+          </div>
+          <div className="skeleton-temp">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="skeleton-day">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+          <div className="skeleton-forecast-icon">
+            <Skeleton count={1} height={50} width={50} />
+          </div>
+          <div className="skeleton-temp">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="skeleton-day">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+          <div className="skeleton-forecast-icon">
+            <Skeleton count={1} height={50} width={50} />
+          </div>
+          <div className="skeleton-temp">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="skeleton-day">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+          <div className="skeleton-forecast-icon">
+            <Skeleton count={1} height={50} width={50} />
+          </div>
+          <div className="skeleton-temp">
+            <Skeleton count={1} height={20} width={50} />
+          </div>
+        </div>
       </div>
     </div>
   );

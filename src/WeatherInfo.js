438b796d4eb2237302a3278daba8fb3ce6b4707b
Miho F <85437491+funamioh@users.noreply.github.com>
2022-01-04 19:05:14 +0900

    Correct props value

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,23 +4,25 @@ import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    return (
-          <div className="WeatherInfo">
-            <div className="container">
-              <h1>{props.data.city}</h1>
-              <div className="row mb-4">
-            <div className="col-12 col-sm-6">
+  return (
+    <div className="WeatherInfo">
+      <div className="container">
+        <h1>{props.address}</h1>
+        <div className="row mb-4">
+          <div className="col-12 col-sm-6">
             <FormatDate date={props.data.date} />
-            <li className="text-left text-capitalize">Description: {props.data.description}</li>
-            </div>
-            <div className="col-12 col-sm-6">
+            <li className="text-left text-capitalize">
+              Description: {props.data.description}
+            </li>
+          </div>
+          <div className="col-12 col-sm-6">
             <li className="text-right">Humidity: {props.data.humidity}%</li>
             <li>Wind speed: {props.data.wind}km/h</li>
-            </div>
-            </div>
-            <WeatherIcon code={props.data.icon} size={60} />
-            </div>
-           <WeatherTemperature celsius={props.data.temperature}/>
-           </div>
-    );
+          </div>
+        </div>
+        <WeatherIcon code={props.data.icon} size={60} />
+      </div>
+      <WeatherTemperature celsius={props.data.temperature} />
+    </div>
+  );
 }
